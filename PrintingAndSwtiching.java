57d9980b03c928b81c0e84e24f5118cbacc0d805
SunilBohara <61348668+SunilBohara@users.noreply.github.com>
2020-05-09 14:16:32 +0545

    Update PrintingAndSwtiching.java

--- PrintingAndSwtiching.java
+++ PrintingAndSwtiching.java
@@ -1,14 +1,20 @@
 package CrudOperations;
+
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
 import java.util.Scanner;
+
 public class PrintingAndSwtiching {
+
     private Student student = new Student();
     private Scanner sc = new Scanner(System.in);
     private Connector connector = new Connector();
+
     public void justPrinting() {
         System.out.println("1) ADD STUDENT ");
         System.out.println("2) DELETE STUDENT");
         System.out.println("3) DISPLAY STUDENT");
-        System.out.println("4) EXIT");
+        System.out.println("4) UPDATE STUDENT");
+        System.out.println("5) EXIT");
     }
 
     public void multipleChoices(int choice) {
@@ -22,15 +28,26 @@ public class PrintingAndSwtiching {
                 int age = sc.nextInt();
                 student.settingStudents(fname, lname, age);
                 connector.insertData(student);
-                //System.out.println(student.toString());
                 break;
             case 2:
-                
+                System.out.print("Enter Your Student Id To Delete : ");
+                int delete = sc.nextInt();
+                connector.deleteStudent(delete);
                 break;
             case 3:
                 connector.displayData();
                 break;
             case 4:
+                System.out.println("Enter your Student Id To Updating The Process : ");
+                int studentid = sc.nextInt();
+                System.out.println("1) update first name");
+                System.out.println("2) update last name");
+                System.out.println("3) update age");
+                System.out.println("4) main menu ");
+                System.out.print("Choice : ");
+                int choice1 = sc.nextInt();
+                connector.updateStudent(studentid, choice1);
+            case 5:
                 System.exit(0);
                 break;
             default:
